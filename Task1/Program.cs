147469df7384a725efc330eeea8c4b3e5eb4dcb4
Anastasia_Liska <agni-0808@yandex.ru>
2022-11-02 23:04:57 +0300

    1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+if (num > 99 && num < 1000)
+{
+    res = num/10;
+    res = res%10;
+    System.Console.WriteLine($"{num} -> {res}");
+}
+else 
+{
+    System.Console.WriteLine("Число не является трёхначным");
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿//static void DayOffWeek(int dayWeek);
+Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+
+    switch (num)
+    {
+        case 1:
+          Console.WriteLine("Понельник-НЕТ");
+          break;
+        case 2:
+          Console.WriteLine("Вторник-НЕТ");
+          break;
+        case 3:
+          Console.WriteLine("Среда-НЕТ");
+          break;
+        case 4:
+          Console.WriteLine("Четверг-НЕТ");       
+          break;
+        case 5:
+          Console.WriteLine("Пятница-НЕТ");
+          break;
+        case 6:
+          Console.WriteLine("Суббота-ДA! Выходной!"); 
+          break;      
+        case 7:
+          Console.WriteLine("Воскресенье-ДA! Выходной!");
+          break;
+        default:
+          Console.WriteLine("Число больше 7 или меньше 1");
+          break;
+    }
